3c223147f104da93fdd6c26cf02286f1e3cea6f3
filipe-oliv95 <filipec.oliveira95@gmail.com>
2023-07-28 18:33:48 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -17,10 +17,10 @@ public class UI {
 	
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("□");
 		}
 		else {
-			System.out.print(" ");
+			System.out.print(piece);
 		}
 		System.out.print(" ");
 	}
